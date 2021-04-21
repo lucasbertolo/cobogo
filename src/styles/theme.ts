6fe45aab8e3735011a7665e2f72801658cae4951
lucasbertolo <lucasbertolo2@gmail.com>
2021-04-20 21:09:20 -0300

    Adding fonts and global styles

---
+++ src/styles/theme.ts
@@ -0,0 +1,23 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+  colors: {
+    yellow: {
+      '500': '#fff212',
+      '700': '#ffb812',
+    },
+  },
+  fonts: {
+    heading: 'ABeeZee',
+    body: 'ABeeZee',
+    mono: 'Open Sans',
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'yellow.500',
+        color: 'black.50',
+      },
+    },
+  },
+});
